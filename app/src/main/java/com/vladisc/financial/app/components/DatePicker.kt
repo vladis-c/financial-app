e569dd78253c7e090719c80f44b586a9e1621a42
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2025-04-26 04:53:20 +0300

    Changed date picker to modal

--- app/src/main/java/com/vladisc/financial/app/components/DatePicker.kt
+++ app/src/main/java/com/vladisc/financial/app/components/DatePicker.kt
@@ -1,11 +1,9 @@
 package com.vladisc.financial.app.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
@@ -15,6 +13,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
@@ -23,11 +22,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.vladisc.financial.app.utils.DateUtils
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -85,21 +83,22 @@ fun DatePicker(
         )
 
         if (showDatePicker) {
-            Popup(
+            Dialog(
                 onDismissRequest = { showDatePicker = false },
-                alignment = Alignment.TopStart
+                properties = DialogProperties(
+                    dismissOnClickOutside = true,
+                    usePlatformDefaultWidth = false
+                )
             ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .offset(y = 64.dp)
-                        .shadow(elevation = 4.dp)
-                        .background(MaterialTheme.colorScheme.surface)
-                        .padding(16.dp)
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    tonalElevation = 4.dp,
+                    modifier = Modifier.fillMaxWidth().padding(16.dp)
                 ) {
                     DatePicker(
                         state = datePickerState,
-                        showModeToggle = false
+                        showModeToggle = true,
+
                     )
                 }
             }
